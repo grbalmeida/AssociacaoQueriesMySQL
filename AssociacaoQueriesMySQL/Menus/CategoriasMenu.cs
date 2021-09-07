0b4ad2d6c3b400077bcb4296057d5621f9c6a407
Ribeiro De Almeida <gribeirodeal@dxc.com>
2021-09-07 19:22:57 -0300

    Removendo CRUD de categorias do Program

---
+++ AssociacaoQueriesMySQL/Menus/CategoriasMenu.cs
@@ -0,0 +1,82 @@
+﻿using AssociacaoQueriesMySQL.Database;
+using AssociacaoQueriesMySQL.Extensions;
+using System;
+using System.Collections.Generic;
+
+namespace AssociacaoQueriesMySQL.Menus
+{
+    public static class CategoriasMenu
+    {
+        private static Action _menuInicial;
+        private static string _connectionString;
+
+        private static void Iniciar()
+        {
+            Iniciar(_menuInicial, _connectionString);
+        }
+
+        public static void Iniciar(Action menuInicial, string connectionString)
+        {
+            _menuInicial = menuInicial;
+            _connectionString = connectionString;
+
+            Console.Clear();
+            Console.WriteLine("1 - Listar Categorias");
+            Console.WriteLine("2 - Inserir Categoria");
+            Console.WriteLine("3 - Remover Categoria");
+
+            var opcao = Console.ReadLine();
+
+            var opcoes = new Dictionary<string, Action>
+            {
+                { "1", Listar },
+                { "2", Inserir },
+                { "3", Remover }
+            };
+
+            opcoes.ExecutarOpcao(opcao, _menuInicial);
+        }
+
+        static void Listar()
+        {
+            Console.Clear();
+
+            using var db = new CategoriaRepositorio(_connectionString);
+            db.Listar();
+            db.Dispose();
+
+            Console.ReadKey();
+            Iniciar();
+        }
+
+        private static void Inserir()
+        {
+            Console.Clear();
+            Console.Write("Informe o Id: ");
+            var id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Informe o Nome: ");
+            var nome = Console.ReadLine();
+
+            using var db = new CategoriaRepositorio(_connectionString);
+            db.Inserir(id, nome);
+            db.Dispose();
+
+            Console.ReadKey();
+            Iniciar();
+        }
+
+        static void Remover()
+        {
+            Console.Clear();
+            Console.Write("Informe o Id: ");
+            var id = Convert.ToInt32(Console.ReadLine());
+
+            using var db = new CategoriaRepositorio(_connectionString);
+            db.Remover(id);
+            db.Dispose();
+
+            Console.ReadKey();
+            Iniciar();
+        }
+    }
+}
--- AssociacaoQueriesMySQL/Program.cs
+++ AssociacaoQueriesMySQL/Program.cs
@@ -34,68 +34,10 @@ namespace AssociacaoQueriesMySQL
             {
                 { "1", () => UsuariosMenu.Iniciar(ExibirMenuInicial, _connectionString) },
                 { "2", () => ClientesMenu.Iniciar(ExibirMenuInicial, _connectionString) },
-                { "3", CategoriasMenu }
+                { "3", () => CategoriasMenu.Iniciar(ExibirMenuInicial, _connectionString) }
             };
 
             opcoes.ExecutarOpcao(opcao, ExibirMenuInicial);
-        }
-
-        static void CategoriasMenu()
-        {
-            Console.Clear();
-            Console.WriteLine("1 - Listar Categorias");
-            Console.WriteLine("2 - Inserir Categoria");
-            Console.WriteLine("3 - Remover Categoria");
-
-            var opcao = Console.ReadLine();
-
-            var opcoes = new Dictionary<string, Action>
-            {
-                { "1", ListarCategorias },
-                { "2", InserirCategoria },
-                { "3", RemoverCategoria }
-            };
-
-            opcoes.ExecutarOpcao(opcao, ExibirMenuInicial);
-        }
-
-        static void ListarCategorias()
-        {
-            Console.Clear();
-
-            using var db = new CategoriaRepositorio(_connectionString);
-            db.Listar();
-
-            Console.ReadKey();
-            CategoriasMenu();
-        }
-
-        static void InserirCategoria()
-        {
-            Console.Clear();
-            Console.Write("Informe o Id: ");
-            var id = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Informe o Nome: ");
-            var nome = Console.ReadLine();
-
-            using var db = new CategoriaRepositorio(_connectionString);
-            db.Inserir(id, nome);
-
-            Console.ReadKey();
-            CategoriasMenu();
-        }
-
-        static void RemoverCategoria()
-        {
-            Console.Clear();
-            Console.Write("Informe o Id: ");
-            var id = Convert.ToInt32(Console.ReadLine());
-
-            using var db = new CategoriaRepositorio(_connectionString);
-            db.Remover(id);
-
-            Console.ReadKey();
-            CategoriasMenu();
         }
 
         static void CriarTabelas()
